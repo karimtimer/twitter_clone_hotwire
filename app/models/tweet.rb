f11294825f8366e297fe09c3b28799e235200974
Karim Timer <abdikarimtimer@gmail.com>
2021-01-12 15:56:38 +0000

    core hotwire funcationality done

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -3,7 +3,7 @@ class Tweet < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   broadcasts
-  # after_create_commit -> { broadcast_append_to tweet }
-  # after_destroy_commit -> { broadcast_remove_to tweet }
-  # after_update_commit -> { broadcast_replace_to tweet }
+  # after_create_commit -> { broadcast_append_to self }
+  # after_destroy_commit -> { broadcast_remove_to self }
+  # after_update_commit -> { broadcast_replace_to self }
 end
