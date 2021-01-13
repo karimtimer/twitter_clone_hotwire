65f5ee95b1daac74ddcbc82d84d553882296ed63
Karim Timer <abdikarimtimer@gmail.com>
2021-01-13 13:19:32 +0000

    fixing index page

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -4,7 +4,7 @@ class TweetsController < ApplicationController
   # GET /tweets
   # GET /tweets.json
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.all.reverse
     @tweet = Tweet.new
   end
 
